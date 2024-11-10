3d0b9359a37c41049553d650c1f2ddd98e78b000
houchenchen <202322430614>
2024-11-10 22:52:14 +0800

    试一试断言还有allure serve

--- pytest_1/test_one.py
+++ pytest_1/test_one.py
@@ -1,12 +1,29 @@
-def test_one():
-    assert 1 == 1
-    assert 1 != 2
-    assert 3 > 2
-    assert 3 < 2
-    assert 3 in [1, 2, 3]
-    assert 3 not in [1, 2, 3]
-    assert 3 >= 3
-    assert 3 <= 3
-    assert 3 is 4
-    assert 3 is not 4
+import pytest
 
+def add(x):
+    return x + 2
+
+class Test_Class_Add():
+    def test_add1(self):
+        assert add(2) == 4
+
+    def test_add2(self):
+        assert add(22) == 24
+
+    def test_add3(self):
+        assert add(100) == 102
+
+class Test_Class_In():
+    def test_in(self):
+        a = "Hello World！"
+        b = "Hi"
+        c = "World"
+        assert c in a
+        assert b not in a
+
+    def test_notin(self):
+        a = "Hello World！"
+        b = "Hi"
+        c = "World"
+        assert b in a
+        assert c not in a
